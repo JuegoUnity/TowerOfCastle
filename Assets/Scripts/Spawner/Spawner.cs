8ceda9c31c3e46a5c011e1400ca2139cd0647faf
Ismaelsanc <51043250+Ismaelsanc@users.noreply.github.com>
2022-03-15 16:21:52 +0100

    Object pooler y agrupacion para los spawns

--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -1,33 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-
-public enum SpawnModes{
-
+public enum SpawnModes
+{
     Fixed,
     Random
 }
+
 public class Spawner : MonoBehaviour
 {
-    //Creamos las variables para hacer los diferentes tipos random spawn de los enemigos.
     [Header("Settings")]
     [SerializeField] private SpawnModes spawnMode = SpawnModes.Fixed;
     [SerializeField] private int enemyCount = 10;
     [SerializeField] private GameObject testGO;
-
+    
     [Header("Fixed Delay")]
     [SerializeField] private float delayBtwSpawns;
-
+    
     [Header("Random Delay")]
     [SerializeField] private float minRandomDelay;
-    [SerializeField] private float maxRandomDelay;    
-    
+    [SerializeField] private float maxRandomDelay;
+
     private float _spawnTimer;
     private int _enemiesSpawned;
 
-    // Hacemos que Spawneen enemigos desde el contador. Hasta 10 enemigos. Agregando los segundos que nosotros queramos y aleatoriamente saldran los enemigos.
-    void Update()
+    private ObjectPooler _pooler;
+
+    private void Start()
+    {
+        _pooler = GetComponent<ObjectPooler>();
+    }
+
+    private void Update()
     {
         _spawnTimer -= Time.deltaTime;
         if (_spawnTimer < 0)
@@ -40,13 +47,13 @@ public class Spawner : MonoBehaviour
             }
         }
     }
-    //Creamos el metodo SpawnEnemy para poder instanciar nuestro testGO.
+
     private void SpawnEnemy()
     {
-        Instantiate(testGO, transform.position, Quaternion.identity);
+        GameObject newInstance = _pooler.GetInstanceFromPool();
+        newInstance.SetActive(true);
     }
 
-    //Añadimos un metodo en el que podamos tener un Spawn respetando el contador hasta 10 pero eligiendo nosotros la velocidad a la que spawnearan los enemigos.
     private float GetSpawnDelay()
     {
         float delay = 0f;
@@ -61,7 +68,7 @@ public class Spawner : MonoBehaviour
 
         return delay;
     }
-    //Creamos la funcion para que nos devuelva un tipo de randomTimer
+    
     private float GetRandomDelay()
     {
         float randomTimer = Random.Range(minRandomDelay, maxRandomDelay);
