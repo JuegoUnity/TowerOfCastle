eab8079dfda08fe24a47790a2fe1c5644e448e84
Ismaelsanc <51043250+Ismaelsanc@users.noreply.github.com>
2022-03-16 22:05:14 +0100

    Creacion de torretas y seguimiento hacia enemigos
    Creamos las torretas y hacemos que cuando entren dentro del radio de vision de la torreta esta el siga en movimiento, pero cuando salga de ese radio se quede como al principio sin seguir el movimiento del enemigo

---
+++ Assets/Scripts/Turrets/Turret.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+   [SerializeField] private float attackRange = 3f;
+
+   private bool _gameStarted;
+   private List<Enemy> _enemies;
+   public Enemy CurrentEnemyTarget { get; set; }
+
+   private void Start() 
+   {
+       _gameStarted = true;
+       _enemies = new List<Enemy>();
+   }
+
+   private void Update() 
+   {
+       GetCurrentEnemytarget();
+       RotateTowardsTarget();
+   }
+
+   private void GetCurrentEnemytarget()
+   {
+       if (_enemies.Count <= 0)
+       {
+           CurrentEnemyTarget = null;
+           return;
+       }
+
+       CurrentEnemyTarget = _enemies[0];
+   }
+
+    private void RotateTowardsTarget()
+    {
+        if (CurrentEnemyTarget == null)
+        {
+            return;
+        }
+
+        Vector3 targetPos = CurrentEnemyTarget.transform.position - transform.position;
+        float angle = Vector3.SignedAngle(transform.up, targetPos, transform.forward);
+        transform.Rotate(0f, 0f, angle);
+    }
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+       if (other.CompareTag("Enemy"))
+       {
+            Enemy newEnemy = other.GetComponent<Enemy>();
+            _enemies.Add(newEnemy);      
+       }
+   }
+
+   private void OnTriggerExit2D(Collider2D other) 
+   {
+       if (other.CompareTag("Enemy"))
+       {
+           Enemy enemy = other.GetComponent<Enemy>();
+           if (_enemies.Contains(enemy))
+           {
+               _enemies.Remove(enemy);
+           }
+       }
+   }
+
+   private void OnDrawGizmos() 
+   {
+
+       if (!_gameStarted)
+       {
+           GetComponent<CircleCollider2D>().radius = attackRange;
+       }
+       Gizmos.DrawWireSphere(transform.position, attackRange);
+   }
+
+}
