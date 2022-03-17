53329c9061cdd3b129c1c6ef88da87e6838c3187
Ismaelsanc <51043250+Ismaelsanc@users.noreply.github.com>
2022-03-17 02:40:47 +0100

    Singleton pattern

--- Assets/Scripts/Managers/DamageTextManager.cs
+++ Assets/Scripts/Managers/DamageTextManager.cs
@@ -2,16 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageTextManager : MonoBehaviour
+public class DamageTextManager : Singleton<DamageTextManager>
 {
     public ObjectPooler Pooler { get; set; } 
 
-    public static DamageTextManager Instance;
-
-    private void Awake() 
-    {
-        Instance = this;
-    }
+    
+    
     void Start()
     {
         Pooler = GetComponent<ObjectPooler>();
