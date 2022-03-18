4b5b95212197890d332ca49e04856696bae40192
Ismaelsanc <51043250+Ismaelsanc@users.noreply.github.com>
2022-03-18 19:49:39 +0100

    Panel Achievements

---
+++ Assets/Scripts/Ahievement/Achievement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Achievement")]
+public class Achievement : ScriptableObject
+{
+    public string ID;
+    public string Title;
+    public int ProgressToUnlock;
+    public int GoldReward;
+    public Sprite Sprite;
+}
