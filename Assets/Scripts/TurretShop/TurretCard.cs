521b1e327a682527018b878a006021b1fe5c123f
Ismaelsanc <51043250+Ismaelsanc@users.noreply.github.com>
2022-03-18 03:47:18 +0100

    Cargado de torretas y precio en el panel de torretas
    Añadimos las torretas y su coste en el panel canvas que hicimos posteriormente

--- Assets/Scripts/TurretShop/TurretCard.cs
+++ Assets/Scripts/TurretShop/TurretCard.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class TurretCard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image turretImage;
+    [SerializeField] private TextMeshProUGUI turretCost;
+
+    public void SetupTurretButton(TurretSettings turretSettings)
     {
-        
+        turretImage.sprite = turretSettings.TurretShopSprite;
+        turretCost.text = turretSettings.TurretShopCost.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
