65df66986d01f40c2720c084d763b89b5b733f0b
Ismaelsanc <51043250+Ismaelsanc@users.noreply.github.com>
2022-03-19 13:53:25 +0100

    Load Achievements

---
+++ Assets/Scripts/Ahievement/AchievementCard.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AchievementCard : MonoBehaviour
+{
+    [SerializeField] private Image achievementImage;
+    [SerializeField] private TextMeshProUGUI title;
+    [SerializeField] private TextMeshProUGUI progress;
+    [SerializeField] private TextMeshProUGUI reward;
+
+    public void SetupAchievement(Achievement achievement)
+    {
+        achievementImage.sprite = achievement.Sprite;
+        title.text = achievement.Title;
+        progress.text = achievement.ProgressToUnlock.ToString();
+        reward.text = achievement.GoldReward.ToString();
+    }
+}
---
+++ Assets/Scripts/Managers/AchievementManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementManager : MonoBehaviour
+{
+  [SerializeField] private AchievementCard achievementCardPrefab;
+  [SerializeField] private Transform achievementPanelContainer;
+  [SerializeField] private Achievement[] achievements;
+
+  private void Start() 
+  {
+      LoadAchievements();
+  }
+
+  private void LoadAchievements()
+  {
+      for (int i = 0; i < achievements.Length; i++)
+      {
+          AchievementCard card = Instantiate(achievementCardPrefab, achievementPanelContainer);
+          card.SetupAchievement(achievements[i]);
+      }
+  }
+}
